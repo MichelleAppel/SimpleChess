59d2253ba9b9220e981d086e9c8a89b2d52142a0
Michelle <appel.michelle@gmail.com>
2016-02-23 16:25:48 +0100

    ascii art board printer                     en stukken toevoegen

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,34 +15,34 @@ public class Main {
 
     private static void add_initial_pieces() {
         for(int i = 0; i < BOARD_WIDTH; i ++) {
-            chess_board[1][i] = "1P" + i + "  ";
-            chess_board[BOARD_HEIGHT - 2][i] = "2P" + i + "  ";
+            chess_board[1][i] = "1P" + (i + 1);
+            chess_board[BOARD_HEIGHT - 2][i] = "2P" + (i + 1);
         }
 
-        chess_board[0][0] = "1R1  ";
-        chess_board[0][7] = "1R2  ";
-        chess_board[7][0] = "2R1  ";
-        chess_board[7][7] = "2R2  ";
+        chess_board[0][0] = "1R1";
+        chess_board[0][7] = "1R2";
+        chess_board[7][0] = "2R1";
+        chess_board[7][7] = "2R2";
 
-        chess_board[0][1] = "1Kn1 ";
-        chess_board[0][6] = "1Kn2 ";
-        chess_board[7][1] = "2Kn1 ";
-        chess_board[7][6] = "2Kn2 ";
+        chess_board[0][1] = "1Kn1";
+        chess_board[0][6] = "1Kn2";
+        chess_board[7][1] = "2Kn1";
+        chess_board[7][6] = "2Kn2";
 
-        chess_board[0][2] = "1B1  ";
-        chess_board[0][5] = "1B2  ";
-        chess_board[7][2] = "2B1  ";
-        chess_board[7][5] = "2B2  ";
+        chess_board[0][2] = "1B1";
+        chess_board[0][5] = "1B2";
+        chess_board[7][2] = "2B1";
+        chess_board[7][5] = "2B2";
 
-        chess_board[0][2] = "1B1  ";
-        chess_board[0][5] = "1B2  ";
-        chess_board[7][2] = "2B1  ";
-        chess_board[7][5] = "2B2  ";
+        chess_board[0][2] = "1B1";
+        chess_board[0][5] = "1B2";
+        chess_board[7][2] = "2B1";
+        chess_board[7][5] = "2B2";
 
-        chess_board[0][3] = "1Q   ";
-        chess_board[0][4] = "1K   ";
-        chess_board[7][3] = "2Q   ";
-        chess_board[7][4] = "2K   ";
+        chess_board[0][3] = "1Q";
+        chess_board[0][4] = "1K";
+        chess_board[7][3] = "2Q";
+        chess_board[7][4] = "2K";
     }
 
     // prints out array which contains the chess board
@@ -53,13 +53,13 @@ public class Main {
                     if (chess_board[i][j] == null) {
                         System.out.println(".    ");
                     } else {
-                        System.out.println(chess_board[i][j]);
+                        System.out.println(chess_board[i][j] + "  ");
                     }
                 } else {
                     if (chess_board[i][j] == null) {
                         System.out.print(".    ");
                     } else {
-                        System.out.print(chess_board[i][j]);
+                        System.out.print(chess_board[i][j] + "  ");
                     }
                 }
             }
